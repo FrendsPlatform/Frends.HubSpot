21e7c43ecd67dc97464c1208e3f08585174404bd
Gabriel <gabriel.dearden@frends.com>
2025-08-05 11:35:55 +0300

    Small fixes

--- Frends.HubSpot.CreateContact/Frends.HubSpot.CreateContact/Helpers/ValidationHelper.cs
+++ Frends.HubSpot.CreateContact/Frends.HubSpot.CreateContact/Helpers/ValidationHelper.cs
@@ -1,31 +1,30 @@
 ﻿using System.Net.Mail;
 
-namespace Frends.HubSpot.CreateContact.Helpers
+namespace Frends.HubSpot.CreateContact.Helpers;
+
+/// <summary>
+/// Validation helper methods
+/// </summary>
+public static class ValidationHelper
 {
     /// <summary>
-    /// Validation helper methods
+    /// Validates email address format
     /// </summary>
-    public static class ValidationHelper
+    /// <param name="email">Email address to validate</param>
+    /// <returns>True if valid email format</returns>
+    public static bool IsValidEmail(string email)
     {
-        /// <summary>
-        /// Validates email address format
-        /// </summary>
-        /// <param name="email">Email address to validate</param>
-        /// <returns>True if valid email format</returns>
-        public static bool IsValidEmail(string email)
+        try
         {
-            try
-            {
-                var addr = new MailAddress(email);
-                return addr.Address == email &&
-                       email.Contains('.') &&
-                       email.IndexOf('@') > 0 &&
-                       email.IndexOf('@') < email.LastIndexOf(value: '.');
-            }
-            catch
-            {
-                return false;
-            }
+            var addr = new MailAddress(email);
+            return addr.Address == email &&
+                   email.Contains('.') &&
+                   email.IndexOf('@') > 0 &&
+                   email.IndexOf('@') < email.LastIndexOf(value: '.');
+        }
+        catch
+        {
+            return false;
         }
     }
 }
