e0cfcac93767be8ca8728c9574bdfeecd589aff5
Gabriel <gabriel.dearden@frends.com>
2025-08-11 09:19:34 +0300

    CreateDeal.cs implementation

--- Frends.HubSpot.CreateDeal/Frends.HubSpot.CreateDeal/Frends.HubSpot.CreateDeal.cs
+++ Frends.HubSpot.CreateDeal/Frends.HubSpot.CreateDeal/Frends.HubSpot.CreateDeal.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.ComponentModel;
-using System.Linq;
+using System.Net.Http;
 using System.Threading;
+using System.Threading.Tasks;
 using Frends.HubSpot.CreateDeal.Definitions;
+using Frends.HubSpot.CreateDeal.Helpers;
+using Newtonsoft.Json.Linq;
 
 namespace Frends.HubSpot.CreateDeal;
 
@@ -12,16 +15,15 @@ namespace Frends.HubSpot.CreateDeal;
 public static class HubSpot
 {
     /// <summary>
-    /// HubSpotes the input string the specified number of times.
-    /// [Documentation](https://tasks.frends.com/tasks/frends-tasks/Frends-HubSpot-CreateDeal)
+    /// Creates a deal in HubSpot.
+    /// [Documentation](https://tasks.frends.com/tasks/frends-tasks/Frends-HubSpot-CreateContact)
     /// </summary>
-    /// <param name="input">Essential parameters.</param>
+    /// <param name="input">Input parameters.</param>
     /// <param name="connection">Connection parameters.</param>
     /// <param name="options">Additional parameters.</param>
     /// <param name="cancellationToken">A cancellation token provided by Frends Platform.</param>
-    /// <returns>object { bool Success, string Output, object Error { string Message, dynamic AdditionalInfo } }</returns>
-    // TODO: Remove Connection parameter if the task does not make connections
-    public static Result CreateDeal(
+    /// <returns>Object { bool Success, string Id, Error Error { string Message, Exception AdditionalInfo } }</returns>
+    public static async Task<Result> CreateDeal(
         [PropertyTab] Input input,
         [PropertyTab] Connection connection,
         [PropertyTab] Options options,
@@ -29,49 +31,59 @@ public static class HubSpot
     {
         try
         {
-            // TODO: Do something with connection parameters, e.g., connect to a service.
-            _ = connection.ConnectionString;
+            if (string.IsNullOrWhiteSpace(connection.ApiKey))
+                throw new Exception("API Key is required");
 
-            // Cancellation token should be provided to methods that support it
-            // and checked during long-running operations, e.g., loops
-            cancellationToken.ThrowIfCancellationRequested();
+            if (string.IsNullOrWhiteSpace(connection.BaseUrl))
+                throw new Exception("Base URL is required");
 
-            var output = string.Join(options.Delimiter, Enumerable.Repeat(input.Content, input.Repeat));
+            if (string.IsNullOrWhiteSpace(input.DealData))
+                throw new Exception("DealData is required");
 
-            return new Result
+            JObject dealProperties;
+
+            try
             {
-                Success = true,
-                Output = output,
-                Error = null,
-            };
-        }
-        catch (Exception e) when (e is not OperationCanceledException)
-        {
-            if (options.ThrowErrorOnFailure)
+                dealProperties = JObject.Parse(input.DealData);
+            }
+            catch (Newtonsoft.Json.JsonReaderException ex)
             {
-                if (string.IsNullOrEmpty(options.ErrorMessageOnFailure))
-                    throw new Exception(e.Message, e);
-
-                throw new Exception(options.ErrorMessageOnFailure, e);
+                throw new Exception("Invalid JSON format in DealData", ex);
             }
 
-            var errorMessage = !string.IsNullOrEmpty(options.ErrorMessageOnFailure)
-                ? $"{options.ErrorMessageOnFailure}: {e.Message}"
-                : e.Message;
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {connection.ApiKey}");
 
-            return new Result
+            var payload = new JObject
             {
-                Success = false,
-                Output = null,
-                Error = new Error
-                {
-                    Message = errorMessage,
-                    AdditionalInfo = new
-                    {
-                        Exception = e,
-                    },
-                },
+                ["properties"] = dealProperties,
             };
+
+            var content = new StringContent(payload.ToString(), System.Text.Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync($"{connection.BaseUrl.TrimEnd('/')}/crm/v3/objects/deals", content, cancellationToken);
+
+            var responseContent = await response.Content.ReadAsStringAsync(cancellationToken);
+            var responseJson = JObject.Parse(responseContent);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = responseJson["message"]?.ToString() ?? "Unknown error";
+                throw new Exception($"HubSpot API error: {response.StatusCode} - {error}");
+            }
+
+            var dealId = responseJson["id"]?.ToString();
+
+            if (!string.IsNullOrWhiteSpace(options.AssociateWithContactData))
+            {
+                await AssociateDeal.AssociateDealWithContact(client, connection.BaseUrl, dealId, options.AssociateWithContactData, cancellationToken);
+            }
+
+            return new Result(true, dealId);
+        }
+        catch (Exception ex)
+        {
+            return ErrorHandler.Handle(ex, options.ThrowErrorOnFailure, options.ErrorMessageOnFailure);
         }
     }
 }
---
+++ Frends.HubSpot.CreateDeal/Frends.HubSpot.CreateDeal/Helpers/AssociateDeal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Frends.HubSpot.CreateDeal.Helpers;
+
+/// <summary>
+/// Association helper method
+/// </summary>
+public static class AssociateDeal
+{
+    /// <summary>
+    /// Associates a created deal with a contact.
+    /// </summary>
+    /// <param name="client">Initialized HttpClient with authentication headers.</param>
+    /// <param name="baseUrl">Base Url for the Api.</param>
+    /// <param name="dealId">Id of the deal to associate.</param>
+    /// <param name="contactId">Id of the contact to associate.</param>
+    /// <param name="cancellationToken">A cancellation token provided by Frends Platform.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    public static async Task AssociateDealWithContact(HttpClient client, string baseUrl, string dealId, string contactId, CancellationToken cancellationToken)
+    {
+        var associationPayload = new JObject
+        {
+            ["types"] = new JArray
+            {
+                new JObject
+                {
+                    ["associationCategory"] = "HUBSPOT_DEFINED",
+                    ["associationTypeId"] = 3,
+                },
+            },
+        };
+
+        var content = new StringContent(associationPayload.ToString(), System.Text.Encoding.UTF8, "application/json");
+
+        var response = await client.PutAsync(
+            $"{baseUrl.TrimEnd('/')}/crm/v3/objects/deals/{dealId}/associations/contacts/{contactId}",
+            content,
+            cancellationToken);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            var responseContent = await response.Content.ReadAsStringAsync(cancellationToken);
+            var responseJson = JObject.Parse(responseContent);
+            var error = responseJson["message"]?.ToString() ?? "Unknown error";
+            throw new Exception($"Failed to associate deal with contact: {response.StatusCode} - {error}");
+        }
+    }
+}
