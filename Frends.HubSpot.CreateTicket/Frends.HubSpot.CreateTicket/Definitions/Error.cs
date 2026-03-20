d69c9113d5b192c53d22b383613e4da5cfb850c8
MichalFrends1 <michal.nowakowski@frends.com>
2026-03-20 07:37:12 +0100

    fix workflows

--- Frends.HubSpot.CreateTicket/Frends.HubSpot.CreateTicket/Definitions/Error.cs
+++ Frends.HubSpot.CreateTicket/Frends.HubSpot.CreateTicket/Definitions/Error.cs
@@ -10,7 +10,7 @@ public class Error
     /// <summary>
     /// Summary of the error.
     /// </summary>
-    /// <example>Unable to create ticket.</example>
+    /// <example>Unable to create new ticket.</example>
     public string Message { get; set; }
 
     /// <summary>
